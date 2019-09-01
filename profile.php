f430e316413f04bbba5f19af7b9f0f9c4f30d3ae
ikhlaqmalik13 <42172695+ikhlaqmalik13@users.noreply.github.com>
2019-09-02 01:56:18 +0530

    add new navigation of profiles

--- profile.php
+++ profile.php
@@ -4,17 +4,80 @@
 	<style>
 		body{
 			margin:0px;
+			min-width: 1200px;
+		}
+
+		.profile-links{
+			margin-left:50px;
+			float:left;
+			width:400px;
+			box-shadow:rgba(0, 0, 0, 0.4) 1px 0px 20px 0px, rgba(0, 0, 0, 0.35) 0 0 1px;
+
+			margin-right:20px;
+		}
+
+		.profile-links h1{
+			background-color: rgb(80,80,255);
+			color:white;
+			margin-top: 0px;
+		}
+
+		.links{
+			margin-top:0px;
+			margin-left: 5px;
+			margin-bottom: 5px;
+			width:390px;
+			height:50px;
+			background-color:white;
+			box-shadow:rgba(0, 0, 0, 0.4) 1px 0px 20px 0px, rgba(0, 0, 0, 0.35) 0 0 1px;
+		}
+
+		.photo{
+			float:left;
+			width: 50px;
+			height:50px;
+			margin-left:5px;
+			margin-right:10px;
+			background-color: white;
+		}
+
+		.photo img{
+			border-radius: 50%;
+			width:100%;
+			height:100%;
+		}
+
+		.username{
+			float:left;
+			width: 270px;
+			height:30px;
+			margin-right:10px;
+			margin-top:10px;
+			margin-bottom: 10px;
+			background-color: white;
+
+		}
+		.username b{
+			font-size: 30px;
+			color:black;
+			background-color: rgb(240,240,240);
+	        padding-right:5px;
+			
+		}
+		.username a{
+			text-decoration: none;
 		}
 
 		.container{
-			margin-left:23%;
-			margin-right:23%;
+			float:left;
+			box-shadow:rgba(0, 0, 0, 0.4) 1px 0px 20px 0px, rgba(0, 0, 0, 0.35) 0 0 1px;
 			width:700px;
-			height:630px;
+			height:550px;
 			background-color: white;
 		}
 		.container h2{
 			margin-top:0px;
+			margin-right:10px;
 			padding-left:10px;
 			background: rgb(80,80,255);
 			color:white;
@@ -23,21 +86,24 @@
 		.profile{
 			float:left;
 			width:300px;
-			height:500px;
+			height:400px;
 			margin-left:15px;
 			background-color: white;
 		}
 		.profile img{
+			border-radius: 50%;
 			width:100%;
 			height:100%;
 		}
 		.avatar{
+			border-radius: 50%;
+			margin-left:20px;
 			width:200px;
 			height:200px;
 			background-color: white;
 			border:2px solid black;
 		}
-		.details{
+	    .details{
 			width:270px;
 			height:200px;
 			margin-top:10px;
@@ -59,9 +125,10 @@
 
 		.achievments{
 			margin-top: 0px;
+			margin-right:10px;
 			float:right;
 			width:335px;
-			height:500px;
+			height:400px;
 			background-color: white;
 		}
 
@@ -80,10 +147,33 @@
 		
 	</style>	
 </head>
-<body>
+<body>	
+	<div class="profile-links">
+		<h1>community </h1>
+		<div class="links">
+			<div class="photo">
+				<img src="img/ikhlaq.jpg">
+			</div>
+			<div class="username">
+				<a href="ikhlaq.html"> <b> Ikhlaq Yousuf </b> </a>
+			</div>	
+
+		</div>	
+
+		<div class="links">
+			<div class="photo">
+				<img src="img/fzee.jpg">
+			</div>
+			<div class="username">
+				<a href="basit.html"> <b> Basit Mohammad </b> </a>
+			</div>	
+
+		</div>	
+
+	</div>	
 
 	<div class="container">
-		<h2> ikhlaq mailk </h2>
+		<h2>Ikhlaq Yousuf </h2>
 		<div class="profile">
 			<div class="avatar">
 				<img src="img/ikhlaq.jpg">
@@ -109,8 +199,6 @@
 
     </div>		
 
-
-
-
+ 
 </body>	
 
